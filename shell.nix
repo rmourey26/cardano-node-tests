7ccc117df50332c991be4f4c24e07d52e2439176
Samuel Leathers <samuel.leathers@iohk.io>
2020-10-01 11:24:03 -0400

    latest changes

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   sources = import ./nix/sources.nix {};
   pkgs = import sources.nixpkgs {};
+  cardano-address = (import sources.cardano-wallet {}).haskellPackages.cardano-addresses.components.exes.cardano-address;
   shell = (import (sources.cardano-node + "/shell.nix") {}).devops.overrideAttrs (oldAttrs: {
     buildInputs = oldAttrs.buildInputs ++ (with pkgs.python3Packages; [
       pytest
@@ -12,6 +13,7 @@ let
       pylint
       mypy
       hypothesis
+      cardano-address
     ]);
   });
 
